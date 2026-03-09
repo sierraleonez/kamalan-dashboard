ab72c0691ec1dc1d31d9e80a2e2b6d97f95ffde8
Almer Andhika <almer@MIFX-IT-ALMER.local>
2026-03-09 10:15:03 +0700

    build: frontend asset

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,5 +1,6 @@
 import { Head, Link, usePage } from '@inertiajs/react';
-import { dashboard, login } from '@/routes';
+import { index } from '@/routes/client/index';
+import { login } from '@/routes'
 import type { SharedData } from '@/types';
 
 export default function Welcome({
@@ -23,7 +24,7 @@ export default function Welcome({
                     <nav className="flex items-center justify-end gap-4">
                         {auth.user ? (
                             <Link
-                                href={dashboard()}
+                                href={index()}
                                 className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                             >
                                 Dashboard
